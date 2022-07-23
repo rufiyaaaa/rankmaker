76b7612950f4ea6225ecb12fff28e287b3cf8311
rufiyaaaa <rufiyaaaa@gmail.com>
2022-07-24 00:11:16 +0900

    Revert "no message"
    This reverts commit f811606f1ac9822f3a37e23840d6976ed20ec017.

--- rankmaker/settings.py
+++ rankmaker/settings.py
@@ -12,9 +12,8 @@ INSTALLED_APPS += [
 ]
 
 # 許可するホスト名のリスト
-# Hostlist = os.environ.get('ALLOWED_HOSTS').split(",")
-USE_X_FORWARDED_HOST = True
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+Hostlist = os.environ.get('ALLOWED_HOSTS').split(",")
+ALLOWED_HOSTS = Hostlist
 
 # 静的ファイルを配置する場所
 STATIC_ROOT = '/usr/share/nginx/html/static'
